b8cb72af8e84cf691aff67dfec9e622dfea02735
Ermir <ermirmehukaj22@gmail.com>
2024-02-19 18:37:44 +0100

    first commit

--- m1-generics/src/Cydeo/Main.java
+++ m1-generics/src/Cydeo/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        ArrayList items = new ArrayList<>();
+        ArrayList<Integer> items = new ArrayList<>();
         items.add(1);
         items.add(2);
         items.add(3);
