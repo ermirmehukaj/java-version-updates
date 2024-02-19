23c3f4c8c7afafb2487bb6e2401fa0e77f5fb520
Ermir <ermirmehukaj22@gmail.com>
2024-02-19 18:31:40 +0100

    first commit

---
+++ m1-generics/src/Cydeo/Main.java
@@ -0,0 +1,26 @@
+package Cydeo;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        ArrayList items = new ArrayList<>();
+        items.add(1);
+        items.add(2);
+        items.add(3);
+        items.add(4);
+        items.add("Apple");
+
+        printDoubled(items);
+
+    }
+
+    private static void printDoubled(ArrayList items) {
+    for (Object each : items){
+        System.out.println((Integer)each *2);
+    }
+    }
+}
