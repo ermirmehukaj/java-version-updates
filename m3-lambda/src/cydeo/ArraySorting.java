07be520edbc9aa1e2902582512ba0c34ef8e70af
Ermir <ermirmehukaj22@gmail.com>
2024-02-21 23:41:08 +0100

    Lambda

--- m3-lambda/src/cydeo/ArraySorting.java
+++ m3-lambda/src/cydeo/ArraySorting.java
@@ -1,16 +1,21 @@
 package cydeo;
 
+import java.util.Queue;
+
 public class ArraySorting {
 
     public static void main(String[] args) {
 
 
         ArraySorting as = new ArraySorting();
-        as.sort();
+        QuickSort qs = new QuickSort();
+        BubbleSort bs = new BubbleSort();
+        as.sort(qs);
+        as.sort(bs);
 
     }
 
-    private void sort(){
-        System.out.println("sorting array");
+    private void sort(Sorting sorting){
+       sorting.sort();
     }
 }
