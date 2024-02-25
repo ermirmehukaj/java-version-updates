e7b12d5f4148085073db96166758a26646d3b6fe
Ermir <ermirmehukaj22@gmail.com>
2024-02-25 16:17:57 +0100

    Lambda

--- m3-lambda/src/main/java/cydeo/task/FunctionalInterface.java
+++ m3-lambda/src/main/java/cydeo/task/FunctionalInterface.java
@@ -3,6 +3,8 @@ package cydeo.task;
 import cydeo.Apple;
 import cydeo.ApplePredicate;
 
+import java.util.function.*;
+
 public class FunctionalInterface {
     public static void main(String[] args) {
 
@@ -13,5 +15,30 @@ public class FunctionalInterface {
                 return apple.getWeight()>200;
             }
         };
+
+        System.out.println("PREDICATE");
+        Predicate<Integer> lesserThan = integer -> integer<18;
+        System.out.println(lesserThan.test(20));
+
+        System.out.println("CONSUMER");
+        Consumer<Integer> display = i -> System.out.println(i);
+        display.accept(10);
+
+        System.out.println("BI CONSUMER");
+        BiConsumer<Integer,Integer> addTwo = (x,y)->System.out.println(x+y);
+        addTwo.accept(1,2);
+
+        System.out.println("FUNCTION");
+        Function<String,String> fun = s -> "Hello" + s;
+        System.out.println(fun.apply("Cydeo"));
+
+        System.out.println("BI FUNCTION");
+        BiFunction<Integer,Integer,Integer> func = (x1,x2) -> x1+x2;
+        System.out.println(func.apply(2,3));
+
+        System.out.println("SUPPLIER");
+        Supplier<Double> randonValue = ()-> Math.random();
+        System.out.println(randonValue.get());
+
     }
 }
