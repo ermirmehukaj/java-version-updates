ce1951759ba43ca71a37a245faedf9c4839d637c
Ermir <ermirmehukaj22@gmail.com>
2024-02-28 00:03:22 +0100

    Stream

--- m5-comparator_interface/src/main/java/com/cydeo/AppleTest.java
+++ m5-comparator_interface/src/main/java/com/cydeo/AppleTest.java
@@ -24,6 +24,9 @@ public class AppleTest {
 
         inventory.sort(comparing(Apple::getWeight));
         System.out.println(inventory);
+//Reversed
+        inventory.sort(comparing(Apple::getWeight).reversed());
+        System.out.println(inventory);
 
 
 
