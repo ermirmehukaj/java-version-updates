3bcf5f4fcf90a40068473ab495332f582c7018cc
Ermir <ermirmehukaj22@gmail.com>
2024-03-02 23:49:47 +0100

    Stream methods

--- m6-stream/src/main/java/com/cydeo/FindingMatching.java
+++ m6-stream/src/main/java/com/cydeo/FindingMatching.java
@@ -3,6 +3,9 @@ package com.cydeo;
 import com.cydeo.task.Dish;
 import com.cydeo.task.DishData;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -12,19 +15,18 @@ public class FindingMatching {
 
         System.out.println("ALL MATCH"
         );
-        boolean isHealthy =DishData.getAll().stream().allMatch(dish -> dish.getCalories()<1000);
+        boolean isHealthy = DishData.getAll().stream().allMatch(dish -> dish.getCalories() < 1000);
         System.out.println(isHealthy);
 
         System.out.println("ANY MATCH");
 
-        if(DishData.getAll().stream().anyMatch(Dish::isVegetarian)){
+        if (DishData.getAll().stream().anyMatch(Dish::isVegetarian)) {
             System.out.println("The menu is vegetarian friendly");
-
 
 
             System.out.println("NONE MATCH");
 
-            boolean isHealthy2 = DishData.getAll().stream().noneMatch(dish -> dish.getCalories()>1000);
+            boolean isHealthy2 = DishData.getAll().stream().noneMatch(dish -> dish.getCalories() > 1000);
             System.out.println(isHealthy2);
 
             System.out.println("FIND ANY");
@@ -39,8 +41,32 @@ public class FindingMatching {
 
             System.out.println(dish2.get());
 
-            System.out.println(IntStream.range(0,100).parallel().findAny());
-            System.out.println(IntStream.range(0,100).parallel().findFirst());
+            System.out.println(IntStream.range(0, 100).parallel().findAny());
+            System.out.println(IntStream.range(0, 100).parallel().findFirst());
+
+            List<String> list1 = Arrays.asList("Jhonny", "David", "Jack", "Duke", "Jill", "Dany", "Julia", "Jenish", "Divya");
+            List<String> list2 = Arrays.asList("Jhonny", "David", "Jack", "Duke", "Jill", "Dany", "Julia", "Jenish", "Divya");
+
+          //  Optional<String> findAny = list1.stream().parallel().filter(s -> s.startsWith("D")).findAny();
+          //  Optional<String> findFirst = list2.stream().parallel().filter(s -> s.startsWith("J")).findFirst();
+
+            Optional<String> findAny = list1.stream().filter(s -> s.startsWith("D")).findAny();
+            Optional<String> findFirst = list2.stream().filter(s -> s.startsWith("J")).findFirst();
+
+            System.out.println(findAny.get());
+            System.out.println(findFirst.get());
+
+            System.out.println("MIN AND MAX");
+
+          Optional<Dish> dishMinimum =   DishData.getAll().stream().min(Comparator.comparing(Dish::getCalories));
+
+            System.out.println(dishMinimum.get());
+
+            Optional<Dish> dishMaximum =   DishData.getAll().stream().max(Comparator.comparing(Dish::getCalories));
+
+            System.out.println(dishMaximum.get());
+
         }
+
     }
 }
---
+++ m6-stream/src/main/java/com/cydeo/Reducing.java
@@ -0,0 +1,38 @@
+package com.cydeo;
+
+import com.cydeo.task.Dish;
+import com.cydeo.task.DishData;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class Reducing {
+
+    public static void main(String[] args) {
+
+
+        List<Integer> numbers = Arrays.asList(4,5,3,9);
+
+        int result = numbers.stream().reduce(0,(a,b)->a+b);
+        System.out.println(result);
+
+
+        int result2 = DishData.getAll().stream()
+                .map(Dish::getCalories)
+                .reduce(Integer::sum).get();
+        System.out.println(result2);
+
+
+        System.out.println("Max and min");
+
+      Optional<Integer> min =  numbers.stream().reduce(Integer::min);
+      Optional<Integer> sum=  numbers.stream().reduce(Integer::sum);
+       Optional<Integer> max = numbers.stream().reduce(Integer::max);
+
+
+        System.out.println("min = " + min.get());
+        System.out.println("max = " + max.get());
+        System.out.println("sum = " + sum.get());
+    }
+}
